847e4bf07fed9312e533b91e01981df758cb7baa
jeportie <jeportie@student.42.fr>
2025-04-30 10:03:07 +0000

    push pause

--- ex00/test/class/BureaucratTest.cpp
+++ ex00/test/class/BureaucratTest.cpp
@@ -6,39 +6,42 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 09:54:41 by jeportie          #+#    #+#             */
-/*   Updated: 2025/04/30 09:56:20 by jeportie         ###   ########.fr       */
+/*   Updated: 2025/04/30 10:02:21 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <gtest/gtest.h>
-#include "../../src/class/ClassName.hpp"
+#include "../../src/class/Bureaucrat.hpp"
 
 TEST(TestSuitName, DefaultConstructor)
 {
-    ClassName object;
-    EXPECT_EQ(objArg1, "ExpectedValue");
+    Bureaucrat o;
+    EXPECT_EQ(o.getName(), "default");
+    EXPECT_EQ(o.getGrade(), 75);
 }
 
 TEST(TestSuitName, CopyConstructor)
 {
-    ClassName object;
-    ClassName copy(object);
-    EXPECT_EQ(copyArg1, "ExpectedValue");
+    Bureaucrat o;
+    Bureaucrat c(o);
+    EXPECT_EQ(c.getName(), "default");
+    EXPECT_EQ(c.getGrade(), 75);
 }
 
 TEST(TestSuitName, AssignmentOperator)
 {
-    ClassName object;
-    ClassName copy(object);
-    copy = original;
-    EXPECT_EQ(copyGetterArg1(), "ExpectedValue");
+    Bureaucrat o("jerome", 10);
+    Bureaucrat c;
+    c = o;
+    EXPECT_EQ(c.getName(), "default");
+    EXPECT_EQ(c.getGrade(), 10);
 }
 
-TEST(TestSuitName, MethodeName)
-{
-    ClassName object;
-    testing::internal::CaptureStdout();
-    obj.methodTested();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "ExpectedValue");
-}
+/*TEST(TestSuitName, MethodeName)*/
+/*{*/
+/*    Bureaucrat object;*/
+/*    testing::internal::CaptureStdout();*/
+/*    obj.methodTested();*/
+/*    std::string output = testing::internal::GetCapturedStdout();*/
+/*    EXPECT_EQ(output, "ExpectedValue");*/
+/*}*/
